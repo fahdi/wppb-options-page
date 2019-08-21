1e4faa0bdc8ecaff67f9bb76f8383858ca3b2867
Fahad Murtaza <fahdim@gmail.com>
2019-08-22 03:56:58 +0400

    Removed some redundant code and cleaned up ased on PHP and WP standards

--- admin/class-options-page-example-admin.php
+++ admin/class-options-page-example-admin.php
@@ -27,7 +27,7 @@ class Options_Page_Example_Admin {
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
+	 * @var      string $plugin_name The ID of this plugin.
 	 */
 	private $plugin_name;
 
@@ -36,21 +36,22 @@ class Options_Page_Example_Admin {
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
+	 * @var      string $version The current version of this plugin.
 	 */
 	private $version;
 
 	/**
 	 * Initialize the class and set its properties.
 	 *
+	 * @param string $plugin_name The name of this plugin.
+	 * @param string $version The version of this plugin.
+	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
 	 */
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+		$this->version     = $version;
 
 	}
 
@@ -73,7 +74,7 @@ class Options_Page_Example_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/options-page-example-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/options-page-example-admin.css', [], $this->version, 'all' );
 
 	}
 
@@ -96,13 +97,14 @@ class Options_Page_Example_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/options-page-example-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/options-page-example-admin.js', [ 'jquery' ], $this->version, false );
 
 	}
 
 
 	/**
-	 * Register the administration menu for this plugin into the WordPress Dashboard menu.
+	 * Register the administration menu for this plugin into the WordPress
+	 * Dashboard menu.
 	 *
 	 * @since    1.0.0
 	 */
@@ -118,9 +120,13 @@ class Options_Page_Example_Admin {
 		 *
 		 * @link https://codex.wordpress.org/Function_Reference/add_options_page
 		 */
-		add_submenu_page( 'plugins.php', 'Plugin settings page title', 'Admin area menu slug', 'manage_options', $this->plugin_name, array($this, 'display_plugin_setup_page')
+		add_submenu_page( 'plugins.php', 'Plugin settings page title', 'Admin area menu slug', 'manage_options', $this->plugin_name, [
+				$this,
+				'display_plugin_setup_page',
+			]
 		);
 	}
+
 	/**
 	 * Add settings action link to the plugins page.
 	 *
@@ -130,11 +136,13 @@ class Options_Page_Example_Admin {
 		/*
 		*  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
 		*/
-		$settings_link = array(
+		$settings_link = [
 			'<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __( 'Settings', $this->plugin_name ) . '</a>',
-		);
-		return array_merge(  $settings_link, $links );
+		];
+
+		return array_merge( $settings_link, $links );
 	}
+
 	/**
 	 * Render the settings page for this plugin.
 	 *
@@ -143,20 +151,28 @@ class Options_Page_Example_Admin {
 	public function display_plugin_setup_page() {
 		include_once( 'partials/' . $this->plugin_name . '-admin-display.php' );
 	}
+
 	/**
-	 * Validate fields from admin area plugin settings form ('exopite-lazy-load-xt-admin-display.php')
-	 * @param  mixed $input as field form settings form
+	 * Validate fields from admin area plugin settings form
+	 *
+	 * @param mixed $input as field form settings form
+	 *
 	 * @return mixed as validated fields
 	 */
-	public function validate($input) {
-		$valid = array();
+	public function validate( $input ) {
+		$valid = [];
 		$valid['example_checkbox'] = ( isset( $input['example_checkbox'] ) && ! empty( $input['example_checkbox'] ) ) ? 1 : 0;
 		$valid['example_text'] = ( isset( $input['example_text'] ) && ! empty( $input['example_text'] ) ) ? esc_attr( $input['example_text'] ) : 'default';
-		$valid['example_select'] = ( isset($input['example_select'] ) && ! empty( $input['example_select'] ) ) ? esc_attr($input['example_select']) : 1;
+		$valid['example_select'] = ( isset( $input['example_select'] ) && ! empty( $input['example_select'] ) ) ? esc_attr( $input['example_select'] ) : 1;
+
 		return $valid;
 	}
+
 	public function options_update() {
-		register_setting( $this->plugin_name, $this->plugin_name, array( $this, 'validate' ) );
+		register_setting( $this->plugin_name, $this->plugin_name, array(
+			$this,
+			'validate',
+		) );
 	}
 
 }

e93a6ca5b2c4bf216a9156ac0774a2ffdc6c0fa1
Fahad Murtaza <fahdim@gmail.com>
2019-08-22 04:04:18 +0400

    Some updates to the settings link code for the plugin

--- includes/class-options-page-example.php
+++ includes/class-options-page-example.php
@@ -165,7 +165,6 @@ class Options_Page_Example {
 
 		// Add Settings link to the plugin
 		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
-
 		$this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
 
 	}
